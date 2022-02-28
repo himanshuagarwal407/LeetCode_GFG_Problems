9000aed632d0fb8290c4a0fd183a60ce94b3a3cf
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-03-01 00:07:28 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.99%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         int a=1,b=1;
         
-        for(int i=0; i<n-1; i++)
+        for(int i=n-2; i>=0; i--)
         {
             int p=a;
             a=a+b;
