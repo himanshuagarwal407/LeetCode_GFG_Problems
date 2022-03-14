8e761d544a78264f616c28565af1b287cd722279
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-03-14 22:40:56 +0530

    Time: 849 ms (6.82%), Space: 168.4 MB (5.01%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    
     vector<vector<int>> ans;
     
-    void helper(int n, int k, vector<int> temp, int index)
+    void combination(int n , int k, vector<int> v, int ind)
     {
-        if(temp.size() == k)
+        if(v.size()==k)
+            ans.push_back(v);
+        
+        for(int i=ind; i<=n; i++)
         {
-            ans.push_back(temp);
-            return;
+            v.push_back(i);
+            combination(n, k, v, i+1);
+            v.pop_back();
         }
         
-        for(int i=index; i<=n; i++)
-        {
-            temp.push_back(i);
-            helper(n, k, temp, i+1);
-            temp.pop_back();
-        }
+        
     }
     
-    vector<vector<int>> combine(int n, int k) 
-    {
-        vector<int> temp;
-        helper(n, k, temp, 1);
-		
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        
+        combination(n, k, v, 1);
+        
         return ans;
     }
 };
