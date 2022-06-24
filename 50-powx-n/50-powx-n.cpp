3b6a8f35fc7d7d0405ff65f0c77f4f1646fdc4ce
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-06-24 23:10:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.11%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans=1;
+        long long nn=n;
+        
+        if(nn<0)
+            nn *= -1;
+        
+        while(nn!=0)
+        {
+            if(nn%2==0)
+            {
+                x*=x;
+                nn/=2;
+            }
+            else
+            {
+                ans *= x;
+                nn--;
+            }
+        }
+        
+        if(n<0)
+            return 1/ans;
+        
+        return ans;
     }
 };
