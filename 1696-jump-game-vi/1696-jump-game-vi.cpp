a5d85be8003fff18ece2b5b5e5c20747d0216536
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-07-09 12:58:48 +0530

    Time: 258 ms (68.64%), Space: 80.6 MB (74.64%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int solveHeap(vector<int>& nums, int k, int n, vector<int> &dp)
+    {  
+        dp[0] = nums[0];
+
+        deque<int> q = {0};
+
+        for(int i = 1; i < n; i++) {
+
+            if(q.front() < i - k){
+                q.pop_front(); 
+            }
+
+            dp[i] = nums[i] + dp[q.front()];    
+
+            while(!q.empty() && dp[q.back()] <= dp[i]){
+                q.pop_back();
+            }   
+
+            q.push_back(i);                 
+        }
+
+        return dp[n-1];
+
+    }
+    
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> dp(n);
+        
+        return solveHeap(nums, k, n, dp);
+    }
+};
