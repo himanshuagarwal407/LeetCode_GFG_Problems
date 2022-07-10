4897b89a36fda86d3bacd8940c0ea96e21ee4f64
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-07-10 14:53:23 +0530

    Time: 11 ms (53.51%), Space: 13.8 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -9,10 +9,8 @@ public:
         dp[1]=0;
         
         for(int i=2; i<dp.size(); i++)
-        {
             dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
-        }
-        
+
         return dp[n];
     }
 };
