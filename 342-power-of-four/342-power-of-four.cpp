3f637446b9bf70db18a94d1845b1ee72b5367617
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-09-08 13:52:19 +0530

    Time: 7 ms (12.72%), Space: 6 MB (27.83%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0)
+        if(n<=0)
             return false;
         
-        while(1)
-        {
-            if(n==1 || n==4)
-                return true;
-            if(n%4!=0)
-                return false;
-            
-            n/=4;
-        }
-        
-        return false;
+        return !(n&(n-1)) && !(n & 0xAAAAAAAA);
     }
 };
