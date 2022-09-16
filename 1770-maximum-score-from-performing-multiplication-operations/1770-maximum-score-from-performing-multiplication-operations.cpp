0b5c2ef096ad934bfa549fa96837ba52cd469df3
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-09-16 13:42:50 +0530

    Time: 709 ms (47.80%), Space: 119.8 MB (72.63%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,24 @@
 class Solution {
-public:
-    int solve(vector<int>& nums, vector<int>& multi, int l, int n, int  ind, int m, vector<vector<int>>& dp)
-    {
-        if(ind==m)
-            return 0;
-           
-        if(dp[l][ind]!=INT_MIN)
-            return dp[l][ind];
-        
-        int a = nums[l]*multi[ind] + solve(nums, multi, l+1, n, ind+1, m, dp);
-        int b = nums[n-1-(ind-l)]*multi[ind] + solve(nums, multi, l, n, ind+1, m, dp);
-        
-        return dp[l][ind]=max(a, b);
-    }
-    
+public:    
     int maximumScore(vector<int>& nums, vector<int>& multi) {
         int n=nums.size();
         int m=multi.size();
                 
-        vector<vector<int>> dp(m+1, vector<int>(m+1, INT_MIN));
-        return solve(nums, multi, 0, n, 0, m, dp);
+        vector<vector<int>> dp(m+1, vector<int>(m+1, 0));
+        
+        for(int ind=m-1; ind>=0; ind--)
+        {
+            for(int l=ind; l>=0; l--)
+            {
+                int r = n-1-(ind-l);
+                
+                int a = nums[l]*multi[ind] + dp[l+1][ind+1];
+                int b = nums[r]*multi[ind] + dp[l][ind+1];
+                
+                dp[l][ind]=max(a,b);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
